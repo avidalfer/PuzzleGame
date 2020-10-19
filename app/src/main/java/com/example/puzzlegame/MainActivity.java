b8bd992a92d4482f834cd99ebeff28ff3185af21
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2020-10-19 11:56:14 +0200

    AppBar first config. BaseActivity contains the whole onclick logic for AppBar items.

--- app/src/main/java/com/example/puzzlegame/MainActivity.java
+++ app/src/main/java/com/example/puzzlegame/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.puzzlegame;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.puzzlegame.common.Utils;
+import com.example.puzzlegame.ui.common.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = Utils.createToolbar(this);
+        ActionBar actionBar = Utils.configDefaultAppBar(this);
     }
 }
