43c49e5ee125f9947051d227c8235656ac9f5c48
Antonio González <58194284+AgongarDev@users.noreply.github.com>
2020-10-22 10:50:22 +0200

    Udt SelectLevelActivity - checkUserLvl

--- app/src/main/java/com/example/puzzlegame/ui/SelectLevelActivity.java
+++ app/src/main/java/com/example/puzzlegame/ui/SelectLevelActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +14,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.puzzlegame.R;
 import com.example.puzzlegame.common.Utils;
+import com.example.puzzlegame.models.User;
 import com.example.puzzlegame.ui.common.BaseActivity;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
@@ -24,7 +27,9 @@ import java.util.logging.Level;
 public class SelectLevelActivity extends BaseActivity {
 
     private Switch[] switches;
-    private Integer level;
+    private int userlvl;
+    private int selectedLvl;
+    private User user;
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     @Override
@@ -36,6 +41,8 @@ public class SelectLevelActivity extends BaseActivity {
         Utils.createToolbar(this);
         Utils.configDefaultAppBar(this);
 
+        userlvl = 3;//user.getUserLvl().getId();
+
         final ViewGroup levelChoseLayout = findViewById(R.id.levelChooseLayout);
 
         final Switch easySW = levelChoseLayout.findViewById(R.id.easy_switch);
@@ -43,30 +50,46 @@ public class SelectLevelActivity extends BaseActivity {
         final Switch hardSW = levelChoseLayout.findViewById(R.id.hard_switch);
 
         switches = new Switch[] { easySW, mediumSW, hardSW };
+
+        allowsAsUserLevel(switches);
     }
+
+    /**
+     * Set enabled switches to match user level
+     * case 1 is always enabled. Don't use break to get cascade behaviour
+     * @param switches
+     */
+    private void allowsAsUserLevel(Switch[] switches) {
+
+        switch (userlvl) {
+            case 3: switches[2].setEnabled(true);
+            case 2: switches[1].setEnabled(true);
+        }
+    }
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     public void setLevelGame (View view) {
 
         Switch sw = (Switch) view;
-        setAllSwitchesOff();
+        setAllSwitchesOn(false);
 
         switch (sw.getId()){
             case R.id.easy_switch:
-                level = 1;
+                selectedLvl = 1;
                 break;
             case R.id.medium_switch:
-                level = 2;
+                selectedLvl = 2;
                 break;
             case R.id.hard_switch:
-                level = 3;
+                selectedLvl = 3;
                 break;
         }
         sw.setChecked(true);
     }
 
-    private void setAllSwitchesOff() {
+    private void setAllSwitchesOn(boolean in) {
         for (Switch sw : switches) {
-            sw.setChecked(false);
+            sw.setChecked(in);
         }
     }
 
@@ -76,7 +99,7 @@ public class SelectLevelActivity extends BaseActivity {
         //enviamos con un intent la dificultad seleccionada para crear la partida más tarde
 
         //Intent intent = new Intent(this, ImageSelectionActivity.class);
-        //intent.putExtra("levelInt", level);
+        //intent.putExtra("levelInt", selectedLevel);
         //startActivity(intent);
     }
 }
