7d9e5d1a9c0460fc480ba44d96a6e727fd0d0098
AnaIgsan <61803230+AnaIgsan@users.noreply.github.com>
2020-11-02 20:50:16 +0100

    añadida funcionalidad al boton play para ir a puzzlegameactivity

--- app/src/main/java/com/example/puzzlegame/PushPlay.java
+++ app/src/main/java/com/example/puzzlegame/PushPlay.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.puzzlegame.R;
+import com.example.puzzlegame.ui.game.PuzzleGameActivity;
 
 public class PushPlay extends AppCompatActivity {
     AnimationDrawable loadingAnimation;
     ImageView loadingImage;
+    Button pushPlayButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,13 @@ public class PushPlay extends AppCompatActivity {
         loadingImage = (ImageView) findViewById(R.id.loadingView);
         loadingImage.setBackgroundResource(R.drawable.loading);
         loadingAnimation = (AnimationDrawable) loadingImage.getBackground();
+        pushPlayButton=(Button) findViewById(R.id.pushPlayButton);
+        pushPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Help.this,PuzzleGameActivity.class));
+            }
+        });
     }
     @Override
     protected void onResume() {
