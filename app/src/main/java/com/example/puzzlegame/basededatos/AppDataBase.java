62e6a8d5b69625634d1087c7b07b911df49f7bd9
avidalfer <avidalfer@gmail.com>
2020-11-02 13:31:01 +0100

    notaciones para room

--- app/src/main/java/com/example/puzzlegame/basededatos/AppDataBase.java
+++ app/src/main/java/com/example/puzzlegame/basededatos/AppDataBase.java
@@ -8,7 +8,7 @@ import com.example.puzzlegame.model.User;
 import com.example.puzzlegame.model.interfaces.DAO.UserDAO;
 
 @Database(entities = {User.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
+public abstract class AppDataBase extends RoomDatabase {
     public abstract UserDAO userDAO();
 
 }
